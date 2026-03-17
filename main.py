cfabec16237ecf81d956a7040bdf42bfaa356f33
NestorBorter <nesbo@gmx.ch>
2026-03-17 23:01:32 +0100

    Added first CSS

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # Creates App with FastAPI
 app = FastAPI()
+
+# CSS
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Templatesfolder
 templates = Jinja2Templates(directory="templates")
